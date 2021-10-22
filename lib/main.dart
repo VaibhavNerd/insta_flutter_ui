9fa25cab9c749a6c77aa68498288c2c2d2f15ccf
vaibh <vaibhavdudeperfect@gmail.com>
2021-10-22 10:51:07 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:insta_flutter/pages/activity_page.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:insta_flutter/pages/home_page.dart';
+import 'package:insta_flutter/pages/root_app.dart';
+import 'package:insta_flutter/pages/sign_in_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+  );
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SplashScreen(
+
+      seconds: 5,
+      backgroundColor: Colors.white,
+
+      image: Image.network(
+          "https://i.ibb.co/tCWs4sT/Instagram-app-logo.jpg"),
+      loaderColor: Colors.white,
+      photoSize: 160,
+      navigateAfterSeconds: SignInPage(),
+    );
+  }
+}
